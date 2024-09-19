8c617e5494ec175aa542f291f53397071a70d91e
Andrew Rovniy <smart.fz@mail.ru>
2024-09-19 14:30:06 +0200

    Add stats page

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import { category } from './category'
 import { owner } from './owner'
 import { firebase } from './firebase'
 import { transactions } from "./transactions";
-import { datetime } from "~/config/datetime";
+import { datetime } from "./datetime";
 
 export const config = {
     isDev: true,
